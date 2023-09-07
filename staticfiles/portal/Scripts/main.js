06413eafaf91acf26cda86ce7d2b7bdcaaeb8fe0
Vijay Varadarajan <vveejaay11@yahoo.com>
2023-09-07 22:55:38 +0530

    leave confirmation added

--- staticfiles/portal/Scripts/main.js
+++ staticfiles/portal/Scripts/main.js
@@ -31,3 +31,26 @@ window.onscroll = () => {
   });
 };
 
+
+jQuery(document).ready(function($){
+	$('.popup-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.popup').addClass('is-visible');
+	});
+	
+
+	$('.popup').on('click', function(event){
+		if( $(event.target).is('.popup-close') || $(event.target).is('.popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+
+
+  $(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.popup').removeClass('is-visible');
+	    }
+    });
+});
+
